8f3256d9f4b5972269a5b62b9e97db0744b7e25b
Gavin Kemp <gpk@gavin-kemp.com>
2020-04-26 19:02:45 +0100

    Choose images sizes and Msgs as type

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -2,19 +2,25 @@ module PhotoGroove exposing (main)
 
 import Array exposing (Array)
 import Browser
-import Html exposing (Html, div, h1, img, text)
+import Html exposing (Html, div, h1, img, text, button, label, input, h3)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
 
 
 type alias Photo =
     { url : String }
 
 
-type alias Msg =
-    { description : String
-    , data : String
-    }
+type Msg
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
 
 
 urlPrefix =
@@ -24,6 +30,7 @@ urlPrefix =
 type alias Model =
     { photos : List Photo
     , selectedUrl : String
+    , chosenSize : ThumbnailSize
     }
 
 
@@ -35,12 +42,34 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
     }
 
 
 photoArray : Array Photo
 photoArray =
     Array.fromList initialModel.photos
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+viewSizeChooser : ThumbnailSize -> ThumbnailSize -> Html Msg
+viewSizeChooser chosenSize size =
+    label []
+        [ input [ type_ "radio", name "size", onClick (ClickedSize size), checked (size == chosenSize) ] []
+        , text (sizeToString size)
+        ]
 
 
 viewThumbnail : String -> Photo -> Html Msg
@@ -50,7 +79,7 @@ viewThumbnail selectedUrl thumb =
         , classList
             [ ( "selected", selectedUrl == thumb.url )
             ]
-        , onClick { description = "ClickedPhoto", data = thumb.url }
+        , onClick (ClickedPhoto thumb.url)
         ]
         []
 
@@ -59,7 +88,11 @@ view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ] (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , button [ onClick ClickedSurpriseMe ]
+            [ text "Surprise me!" ]
+        , h3 [] [ text "Thumbnail size:" ]
+        , div [ id "choose-size" ] (List.map (viewSizeChooser model.chosenSize) [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ] (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
             [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedUrl)
@@ -68,11 +101,27 @@ view model =
         ]
 
 
+getPhotoUrl : Int -> String
+getPhotoUrl index =
+    case Array.get index photoArray of
+        Just photo ->
+            photo.url
+
+        Nothing ->
+            ""
+
+
+update : Msg -> Model -> Model
 update msg model =
-    if msg.description == "ClickedPhoto" then
-        { model | selectedUrl = msg.data }
-    else
-        model
+    case msg of
+        ClickedPhoto url ->
+            { model | selectedUrl = url }
+
+        ClickedSize size ->
+            { model | chosenSize = size }
+
+        ClickedSurpriseMe ->
+            { model | selectedUrl = "2.jpeg" }
 
 
 main =
